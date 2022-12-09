4d80636f15a1c06a0d7c6edbfadd019fc89b1416
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2022-12-09 11:54:01 -0800

    wip

--- tests/cache/0e87c1212a698494dcdb198af3e0eb2f.php
+++
@@ -1,1 +0,0 @@
-<h2>Hello from <?php echo @htmlspecialchars($name); ?> and value = <?php echo @htmlspecialchars($value); ?></h2><?php /** __template_path__:  */ ?>
--- tests/cache/c61404957758dfda283709e89376ab3e.php
+++
@@ -1,7 +0,0 @@
-<?php $this->startSection('body'); ?>
-
-    <h2>This is the sub layout</h2>
-
-    <?php echo $this->sections['content'] ?? ""; ?>
-    
-<?php $this->endSection(); ?><?php $this->extends('base'); ?><?php /** __template_path__:  */ ?>
